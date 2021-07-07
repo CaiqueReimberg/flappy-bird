ae62d286e231f238881719e62372b9a5e5b75fbe
Caique Reimberg <caiquecreimberg@gmail.com>
2021-07-07 18:08:45 -0300

    Added sprite and loop

--- jogo.js
+++ jogo.js
@@ -4,90 +4,88 @@ const sprites = new Image();
 sprites.src = './sprites.png';
 
 const canvas = document.querySelector('canvas');
-const contexto = canvas.getContext('2d');
-
-
-// [Plano de Fundo]
-const planoDeFundo = {
-  spriteX: 390,
-  spriteY: 0,
-  largura: 275,
-  altura: 204,
-  x: 0,
-  y: canvas.height - 204,
-  desenha() {
-    contexto.fillStyle = '#70c5ce';
-    contexto.fillRect(0,0, canvas.width, canvas.height)
-
-    contexto.drawImage(
-      sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
-      planoDeFundo.largura, planoDeFundo.altura,
-      planoDeFundo.x, planoDeFundo.y,
-      planoDeFundo.largura, planoDeFundo.altura,
-    );
-
-    contexto.drawImage(
-      sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
-      planoDeFundo.largura, planoDeFundo.altura,
-      (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
-      planoDeFundo.largura, planoDeFundo.altura,
-    );
-  },
-};
-
-// [Chao]
-const chao = {
-  spriteX: 0,
-  spriteY: 610,
-  largura: 224,
-  altura: 112,
-  x: 0,
-  y: canvas.height - 112,
-  desenha() {
-    contexto.drawImage(
-      sprites,
-      chao.spriteX, chao.spriteY,
-      chao.largura, chao.altura,
-      chao.x, chao.y,
-      chao.largura, chao.altura,
-    );
-
-    contexto.drawImage(
-      sprites,
-      chao.spriteX, chao.spriteY,
-      chao.largura, chao.altura,
-      (chao.x + chao.largura), chao.y,
-      chao.largura, chao.altura,
-    );
-  },
-};
+const context = canvas.getContext('2d');
 
 const flappyBird = {
   spriteX: 0,
   spriteY: 0,
-  largura: 33,
-  altura: 24,
+  width: 33,
+  height: 24,
   x: 10,
   y: 50,
-  desenha() {
-    contexto.drawImage(
+  draw() {
+    context.drawImage(
       sprites,
-      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
-      flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Y beginning
+      flappyBird.width, flappyBird.height, // Size of sprite recort
       flappyBird.x, flappyBird.y,
-      flappyBird.largura, flappyBird.altura,
+      flappyBird.width, flappyBird.height,
     );
   }
-}
+};
+
+// Background
+const background = {
+  spriteX: 390,
+  spriteY: 0,
+  width: 275,
+  height: 204,
+  x: 0,
+  y: canvas.height - 204,
+  draw() {
+    context.fillStyle = '#70c5ce';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.drawImage(
+      sprites,
+      background.spriteX, background.spriteY, // Sprite X, Y beginning
+      background.width, background.height, // Size of sprite recort
+      background.x, background.y,
+      background.width, background.height,
+    );
+
+    context.drawImage(
+      sprites,
+      background.spriteX, background.spriteY, // Sprite X, Y beginning
+      background.width, background.height, // Size of sprite recort
+      background.x + background.width, background.y,
+      background.width, background.height,
+    );
+  }
+};
+
+// Ground
+const ground = {
+  spriteX: 0,
+  spriteY: 610,
+  width: 224,
+  height: 112,
+  x: 0,
+  y: canvas.height - 112,
+  draw() {
+    context.drawImage(
+      sprites,
+      ground.spriteX, ground.spriteY, // Sprite X, Y beginning
+      ground.width, ground.height, // Size of sprite recort
+      ground.x, ground.y,
+      ground.width, ground.height,
+    );
+
+    // To extend ground size
+    context.drawImage(
+      sprites,
+      ground.spriteX, ground.spriteY, // Sprite X, Y beginning
+      ground.width, ground.height, // Size of sprite recort
+      ground.x + ground.width, ground.y,
+      ground.width, ground.height,
+    );
+  }
+};
 
 function loop() {
-  planoDeFundo.desenha();
-  chao.desenha();
-  flappyBird.desenha();
-
-  flappyBird.y = flappyBird.y + 1;
+  background.draw();
+  ground.draw();
+  flappyBird.draw();
 
   requestAnimationFrame(loop);
 }
